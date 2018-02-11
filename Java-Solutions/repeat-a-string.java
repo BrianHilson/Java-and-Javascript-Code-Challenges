cabc76649148ef456696e37e145032f5bf6c6f00
Brian Hilson <35715304+BrianHilson@users.noreply.github.com>
2018-02-10 23:13:34 -0600

    Create repeat-a-string.java

---
+++ Java-Solutions/repeat-a-string.java
@@ -0,0 +1,7 @@
+    public static String repeatStringNumTimes(String str, int num) {
+        String newStr = "";
+        for (int i = 0; i < num; i++) {
+            newStr += str;
+        }
+        return newStr;
+    }
