d4a44d362f6ec355fd8ffe34735144ecd42551ad
Brian Hilson <35715304+BrianHilson@users.noreply.github.com>
2018-02-20 20:18:41 -0600

    Create ceasars-cipher.js

---
+++ JavaScript-Solutions/ceasars-cipher.js
@@ -0,0 +1,32 @@
+/* From FreeCodeCamp: https://www.freecodecamp.org/challenges/caesars-cipher
+
+Caesars Cipher
+
+"One of the simplest and most widely known ciphers is a Caesar cipher, 
+also known as a shift cipher. In a shift cipher the meanings of the 
+letters are shifted by some set amount.
+
+A common modern use is the ROT13 cipher, where the values of the letters 
+are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔  'O' and so on.
+
+Write a function which takes a ROT13 encoded string as input and 
+returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic 
+character (i.e. spaces, punctuation), but do pass them on." */
+
+
+function rot13(str) {
+  var returnStr = "";
+  for (var i = 0; i < str.length; i++) {
+    var code = str.charCodeAt(i);
+    if (code > 64 && code < 91) {
+      code += 13;
+      if (code > 90) {
+        code -= 26;
+      }
+    }
+    returnStr += String.fromCharCode(code);
+  }
+  return returnStr;
+}
